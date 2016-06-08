d8aff698922941b98d2fd8e6fbfed26da4c870dc
trierra <trierra.dev@gmail.com>
2016-06-08 15:29:07 -0700

    initial

---
+++ WorkoutTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutTableViewCell.swift
+//  SCWorkout
+//
+//  Created by Oksana Chuiko on 6/6/16.
+//  Copyright © 2016 Oksana Chuiko. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
